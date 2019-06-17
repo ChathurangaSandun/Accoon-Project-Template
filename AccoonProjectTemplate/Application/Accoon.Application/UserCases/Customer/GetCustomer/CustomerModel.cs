39090460b3657ff34c9a5da8e01f3b1332c4c621
chathurangasandun <csk@tiqri.com>
2019-06-17 18:24:39 +0300

    customer commands and queries added

---
+++ AccoonProjectTemplate/Application/Accoon.Application/UserCases/Customer/GetCustomer/CustomerModel.cs
@@ -0,0 +1,15 @@
+﻿using Accoon.Application.Interfaces.Automapper;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accoon.Application.UserCases.Customer.GetCustomer
+{
+    public class CustomerModel: IMapFrom<Accoon.Domain.Entities.Customer>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ AccoonProjectTemplate/Application/Accoon.Application/UserCases/Customer/GetCustomerList/GetCustomersListQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using Accoon.Application.Interfaces.Database;
+using Accoon.CQRSCAApi.Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Accoon.Application.UserCases.Customer.GetCustomerList
+{
+    public class GetCustomersListQueryHandler : IRequestHandler<GetCustomersListQuery, CustomerListViewModel>
+    {
+        private readonly IDatabaseContext cqrscaDbContext;
+        private readonly IMapper mapper;
+
+        public GetCustomersListQueryHandler(IDatabaseContext cqrscaDbContext, IMapper mapper)
+        {
+            this.cqrscaDbContext = cqrscaDbContext;
+            this.mapper = mapper;
+        }
+
+        public async Task<CustomerListViewModel> Handle(GetCustomersListQuery request, CancellationToken cancellationToken)
+        {
+            // get from database
+            var customerList = await this.cqrscaDbContext.Customers
+                .ProjectTo<CustomerDetailModel>(this.mapper.ConfigurationProvider).ToListAsync<CustomerDetailModel>(cancellationToken);
+
+            var model =  new CustomerListViewModel()
+            {
+                Customers = customerList
+            };
+
+            return model;
+        }
+    }
+}
