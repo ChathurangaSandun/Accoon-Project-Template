52b163d4eda8640a04a860ca90f77a96b495fb87
chathurangasandun <csk@tiqri.com>
2019-06-18 21:57:38 +0300

    full commands and queries finished

--- AccoonProjectTemplate/Application/Accoon.Application/UserCases/Customer/CreateCustomer/CustomerCreatedHandler.cs
+++ AccoonProjectTemplate/Application/Accoon.Application/UserCases/Customer/CreateCustomer/CustomerCreatedHandler.cs
@@ -9,17 +9,12 @@ namespace Accoon.Application.UserCases.Customer.CreateCustomer
 {
     public class CustomerCreatedHandler : INotificationHandler<CustomerCreated>
     {
-        //private readonly IEventBus eventBus;
+        
         public CustomerCreatedHandler()
         {
-            //this.eventBus = eventBus;
         }
         public Task Handle(CustomerCreated response, CancellationToken cancellationToken)
         {
-            //var customerCreatedEvent = new CustomerCreatedIntegrationEvent(response.CustomerId);
-
-            //this.eventBus.Publish(customerCreatedEvent);
-
             return Task.CompletedTask;
 
         }
---
+++ AccoonProjectTemplate/Persistence/Accoon.Persistence/DatabaseContext/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using Accoon.Application.Interfaces.Database;
+using Accoon.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Accoon.Persistence.DatabaseContext
+{
+    public class DefaultDatabaseContext : DbContext, IDatabaseContext
+    {
+        // constructor
+        public DefaultDatabaseContext(DbContextOptions<DefaultDatabaseContext> options) : base(options)
+        {
+
+        }
+
+        // database entities
+        public DbSet<Customer> Customers { get; set; }
+
+        // register entity configurations
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DefaultDatabaseContext).Assembly);
+        }
+
+
+
+    }
+}
+
+// Add migrations
+// Add-Migration InitMigration -Project Accoon.Persistence -StartupProject Accoon.Api -Context DefaultDatabaseContext
+// update-database -Project Accoon.Persistence -StartupProject Accoon.Api
